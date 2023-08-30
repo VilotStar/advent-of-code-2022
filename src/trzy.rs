1c6f47a11660691e2fc033e2553dbe4ce88fa2f9
ElonGates <59313695+VilotStar@users.noreply.github.com>
2023-08-30 02:04:06 +0100

    popraw gramatykę

--- src/trzy.rs
+++ src/trzy.rs
@@ -6,27 +6,27 @@ publiczny funkcja główny() {
     niech zmienny priorytet_suma: ucałkowita = 0;
 
     niech zmienny priorytet_suma_grupa: ucałkowita = 0;
-    niech zmienny obecna_grupa: [String; 3] = Default::default();
+    niech zmienny obecna_grupa: [Ciąg; 3] = Default::default();
 
-    dla (indeks, plecak) w plecaki.enumerate() {
+    dla (indeks, plecak) w plecaki.wyliczać() {
         niech (przedział_pierwszy, przedział_drugi) = plecak.podział_na(plecak.długość() / 2);
 
         niech znaki_wspólne: znakc = przedział_pierwszy.znakic()
             .filtr(|&znak| przedział_drugi.zawiera(znak))
             .nty(0).odpakuj();
 
-        if znaki_wspólne.jest_niższy() { // ord('a') == 97, 97 - 96 = 1
-            priorytet_suma += znaki_wspólne as ucałkowita - 96;
-        } else {                         // ord('A') == 65, 65 - 64 = 1, ponieważ wielkie litery, 1 + 26 == 27
-            priorytet_suma += znaki_wspólne as ucałkowita - 64 + 26;
+        jeżeli znaki_wspólne.jest_niższy() { // ord('a') == 97, 97 - 96 = 1
+            priorytet_suma += znaki_wspólne jako ucałkowita - 96;
+        } inaczej {                         // ord('A') == 65, 65 - 64 = 1, ponieważ wielkie litery, 1 + 26 == 27
+            priorytet_suma += znaki_wspólne jako ucałkowita - 64 + 26;
         }
 
-        obecna_grupa[indeks % 3] = plecak.to_string();
+        obecna_grupa[indeks % 3] = plecak.do_łańcucha();
         if indeks % 3 != 2 {
-            continue;
+            kontynuować;
         }
 
-        /* let znaki_wspólne_grupa: znakc = obecna_grupa[0].znakic()
+        /* niech znaki_wspólne_grupa: znakc = obecna_grupa[0].znakic()
             .filtr(|&znak| {
                 dla i w 1..3 {
                     if !obecna_grupa[i].zawiera(znak) {
@@ -37,14 +37,14 @@ publiczny funkcja główny() {
             })
             .nty(0).odpakuj(); */
 
-        let znaki_wspólne_grupa: znakc = obecna_grupa[0].znakic()
+        niech znaki_wspólne_grupa: znakc = obecna_grupa[0].znakic()
             .filtr(|&znak| obecna_grupa[1].zawiera(znak) && obecna_grupa[2].zawiera(znak))
             .nty(0).odpakuj(); // szybciej niż w pętli
 
-        if znaki_wspólne.jest_niższy() { // ord('a') == 97, 97 - 96 = 1
-            priorytet_suma_grupa += znaki_wspólne_grupa as ucałkowita - 96;
-        } else {                         // ord('A') == 65, 65 - 64 = 1, ponieważ wielkie litery, 1 + 26 == 27
-            priorytet_suma_grupa += znaki_wspólne_grupa as ucałkowita - 64 + 26;
+        jeżeli znaki_wspólne.jest_niższy() { // ord('a') == 97, 97 - 96 = 1
+            priorytet_suma_grupa += znaki_wspólne_grupa jako ucałkowita - 96;
+        } inaczej {                         // ord('A') == 65, 65 - 64 = 1, ponieważ wielkie litery, 1 + 26 == 27
+            priorytet_suma_grupa += znaki_wspólne_grupa jako ucałkowita - 64 + 26;
         }
     }
 
